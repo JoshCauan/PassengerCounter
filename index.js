8d0d04de15f49bdfa57e9631d3621d4d13678179
Joshua Cauan <godlikestreak681@gmail.com>
2025-11-12 21:50:34 +0800

    Created Welcome Message

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function increment() {
 function save() {
     alert(count)
 }
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById("welcome-el")
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+let name = "Josh"
+let greeting = "Welcome to Passenger Counter "
+// Render the welcome message using welcomeEl.innerText
+
+welcomeEl.innerText = greeting + name

5c26a93395be6b85b3afa84f3cc7aa14d8fdae70
Joshua Cauan <godlikestreak681@gmail.com>
2025-11-13 20:51:24 +0800

    Created Save Button Function

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let welcomeEl = document.getElementById("welcome-el")
+let saveEl = document.getElementById("save-el")
+let name = "Josh"
+let greeting = "Welcome to Passenger Counter "
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
 function save() {
-    alert(count)
+    // alert(count)
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let  prevEntries = count + " - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += prevEntries
+    // NB: Make sure to not delete the existing content of the paragraph
+
 }
 
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById("welcome-el")
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
-let name = "Josh"
-let greeting = "Welcome to Passenger Counter "
-// Render the welcome message using welcomeEl.innerText
+
 
 welcomeEl.innerText = greeting + name
+welcomeEl.innerText += "👋"
